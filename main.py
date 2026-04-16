108e2f2ac1cc9f2944f22ca23b9ac9e77b9eb0ea
sanjaykumarchaudhridss-hash <sanjaykumarchaudhridss@gmail.com>
2026-04-16 08:24:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from pyrogram import Client, filters
 import os
 
-API_ID = int(os.getenv("API_ID"))
+API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_NAME = os.getenv("OWNER_NAME")
 
-app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+if not API_ID or not API_HASH or not BOT_TOKEN:
+    print("❌ Missing Variables!")
+    exit()
+
+app = Client("bot", api_id=int(API_ID), api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("ping", prefixes="."))
 async def ping(client, message):
@@ -14,7 +18,7 @@ async def ping(client, message):
 
 @app.on_message(filters.command("alive", prefixes="."))
 async def alive(client, message):
-    await message.reply(f"✅ Bot Alive\n👑 Owner: {OWNER_NAME}")
+    await message.reply(f"✅ Alive\nOwner: {OWNER_NAME}")
 
-print("Bot Started...")
+print("Bot Started Successfully 🔥")
 app.run()
